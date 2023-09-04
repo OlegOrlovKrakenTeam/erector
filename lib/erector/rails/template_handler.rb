5d348da9d0b8ba618a4e7534caf7e4db4db9927b
Orel Sokolov <orelcokolov@gmail.com>
2023-09-04 17:33:40 +0300

    Add Rails 6 support

--- lib/erector/rails/template_handler.rb
+++ lib/erector/rails/template_handler.rb
@@ -1,13 +1,27 @@
 module Erector
   module Rails
     class TemplateHandler
-      def call(template)
-        require_dependency template.identifier
-        widget_class_name = "views/#{template.identifier =~ %r(views/([^.]*)(\..*)?\.rb) && $1}".camelize
-        is_partial = File.basename(template.identifier) =~ /^_/
-        <<-SRC
+
+      if defined?(ActionView) && defined?(Rails) && Rails.respond_to?(:version) && Rails.version.to_s =~ /^[345]/
+        def call(template)
+          require_dependency template.identifier
+          widget_class_name = "views/#{template.identifier =~ %r(views/([^.]*)(\..*)?\.rb) && $1}".camelize
+          is_partial = File.basename(template.identifier) =~ /^_/
+          <<-SRC
         Erector::Rails.render(#{widget_class_name}, self, local_assigns, #{!!is_partial})
         SRC
+        end
+      end
+
+      if defined?(ActionView) && defined?(Rails) && Rails.respond_to?(:version) && Rails.version.to_s =~ /^[67]/
+        def call(template, source)
+          require_dependency template.identifier
+          widget_class_name = "views/#{template.identifier =~ %r(views/([^.]*)(\..*)?\.rb) && $1}".camelize
+          is_partial = File.basename(template.identifier) =~ /^_/
+          <<-SRC
+        Erector::Rails.render(#{widget_class_name}, self, local_assigns, #{!!is_partial})
+        SRC
+        end
       end
     end
   end
